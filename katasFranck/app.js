1a3385c9714008066ae4df7b0b9f92d7181f44b9
Franck <136079691+Freddyede@users.noreply.github.com>
2024-05-02 10:06:08 +0200

    Remove katas

--- katasFranck/app.js
+++
@@ -1,6 +0,0 @@
-const array = [1, 2, 3, 4, 5];
-
-const addValueToArray = (array, val, index) => array[index === undefined ? array.length : index] = val;
-
-addValueToArray(array, 6, 3);
-console.log(array);
