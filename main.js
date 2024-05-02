6345aca27c367244f232eef1439e902e93c1079f
Franck <136079691+Freddyede@users.noreply.github.com>
2024-05-02 10:05:32 +0200

    update

--- main.js
+++
@@ -1,9 +0,0 @@
-import markdown from './src/data/markdown.json' assert {type: 'json'};
-import { MarkdownHtmlParser } from "./src/abstract/MarkdownHtmlParser.js";
-import { Markdown } from "./src/controllers/Markdown.js";
-
-export {
-    markdown,
-    MarkdownHtmlParser,
-    Markdown
-};
