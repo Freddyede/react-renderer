6ff0376651d1a0391bfa2243eade5564d8109f65
Franck <136079691+Freddyede@users.noreply.github.com>
2024-05-02 13:11:15 +0200

    Adding comment markdown reader

--- src/controllers/Markdown.js
+++ src/controllers/Markdown.js
@@ -8,6 +8,8 @@ export class Markdown extends MarkdownHtmlParser {
 
     /**
      * Charge readBlocs or readLines if this.md is bloc or simple text
+     * @author Patouillard Franck<patouillardfranck.development@gmail.com>
+     * @version 0.0.1
      */
     logicalSystems() {
         if(this.verifyIsMarkdown() && this.md.includes("\n\n")){
@@ -17,12 +19,23 @@ export class Markdown extends MarkdownHtmlParser {
         }
     }
 
+    /**
+     * Run all line `\n\n` element and passe it to readLines
+     * @author Patouillard Franck<patouillardfranck.development@gmail.com>
+     * @version 0.0.1
+     */
     readBlocs() {
         for (const element of this.md.split('\n\n')) {
             this.readLines(element);
         }
     }
 
+    /**
+     * Read all line of markdown text
+     * @param element line markdown
+     * @author Patouillard Franck<patouillardfranck.development@gmail.com>
+     * @version 0.0.1
+     */
     readLines(element) {
         console.log(element);
     }
